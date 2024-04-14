a4831ea83f064fb68eba159ed42fe8b865d062f8
seonbin99 <tst7@naver.com>
2024-04-14 22:48:30 +0900

    메인 효과 줄이고 메인 반응형까지 해서 완료!

--- script/action.js
+++ script/action.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
-    $('.visual_text p span').each(function(index) {
-        $(this).css({ animationDelay: (index * 0.1) + 's' });
-    });
-
+document.querySelector('.hambuger').addEventListener('click', function() {
+    this.classList.toggle('on');
+    var modal = document.querySelector('.modal');
+    var navbox = document.querySelector('.navbox');
+    
+    if (modal.style.opacity === '1' && navbox.classList.contains('on')) {
+        modal.style.opacity = '0';
+        navbox.classList.remove('on');
+        setTimeout(function() {
+            modal.style.display = 'none';
+        }, 300); // CSS의 transition 시간과 동일한 값으로 설정
+    } else {
+        modal.style.display = 'block';
+        setTimeout(function() {
+            modal.style.opacity = '1';
+            navbox.classList.add('on');
+        }, 10); // setTimeout의 최소값
+    }
 });
